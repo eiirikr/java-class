411c8aed88842d9b38fe651bebdbd1f1fbcd7093
javierjericm <javierjericm@gmail.com>
2025-06-28 09:15:23 +0800

    Added Task2

---
+++ Task2.java
@@ -0,0 +1,17 @@
+public class Task2 {
+    public static void main(String[] args) {
+        char h = 'H';
+        byte three = 3;
+        short one = 1;
+        int zero = 0;
+        char space = ' ';
+        char doubleU = 'w';
+        String word = "rld";
+        float twoPointZero = 2.0f;
+        boolean truth = true;
+
+        String output = h + "" + three + "" + one + "" + one + zero + space + doubleU + zero + word + space + twoPointZero + space + truth;
+
+        System.out.println(output);
+    }
+}
